b66c52aa4f4c6554cf861a5d8cbaf3db61192a7e
Lance <234095@shakopeeschools.org>
2019-01-23 12:23:58 -0600

    updated team name
    I updated my team name and strat

--- team8.py
+++ team8.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Lance Lisomdras Team' # Only 10 chars displayed. 
+strategy_name = 'We collude till the other person betrays, then we betray'
+strategy_description = 'Collude till they betray?'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,8 +26,24 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
-
+    
+    if len(my_history)%2==0:
+       return 'c'
+    else:
+       return 'b'
+      
+   
+    if their_history[-1] == 'c':
+       return 'c'
+      
+       if len(their_history) =='c':
+           return 'c'
+       elif len(their_history) =='b':
+           return 'b'
+       elif their_history[-1] == 'b':
+           return 'b'
+       else:
+           return 'c'
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
@@ -53,7 +69,7 @@ if __name__ == '__main__':
               my_score=0,
               their_score=0,
               result='b'):
-         print('Test passed')
+         print ('Test passed')
      # Test 2: Continue betraying if they collude despite being betrayed.
     test_move(my_history='bbb',
               their_history='ccc', 
@@ -65,4 +81,4 @@ if __name__ == '__main__':
               # move('bbb', 'ccc', 0, 0) returns 'b'.
               my_score=0, 
               their_score=0,
-              result='b')             
+              result='b')                          
