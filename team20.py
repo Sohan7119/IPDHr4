7a325df8c0f3da7408f76dc2f438bc0b89ae3b9b
Mason Fox <228264@shakopeeschools.org>
2019-01-23 12:29:46 -0600

    Code
    I added my code

--- team20.py
+++ team20.py
@@ -6,29 +6,26 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+team_name = 'Mason Foxs Team' # Only 10 chars displayed.
+strategy_name = 'Copycat Strategy do what they did the turn before'
 strategy_description = 'How does this strategy decide?'
-    
-def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
+def move(my_history, their_history, my_score, their_score):
+    if len(my_history)/2==0:
+        return 'c'
+    if their_history[-1] == 'c':
+        return 'c'
+    else:
+        return 'b'
+
+        # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
     # The most recent round is my_history[-1] and their_history[-1].
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
-    
+   
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
@@ -65,4 +62,12 @@ if __name__ == '__main__':
               # move('bbb', 'ccc', 0, 0) returns 'b'.
               my_score=0, 
               their_score=0,
-              result='b')             
+              result='b')   
+                  
+def move(my_history, their_history, my_score, their_score):
+    if len(my_history)/2==0:
+        return 'c'
+    if their_history[-1] == 'c':
+        return 'c'
+    else:
+        return 'b'
