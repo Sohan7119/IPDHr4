83bf300914588a8e873078b5199106428ac5c781
JadenEstrem2325 <223121@shakopeeschools.org>
2019-01-23 12:32:36 -0600

    Updated code
    I added my code

--- team21.py
+++ team21.py
@@ -11,12 +11,8 @@ strategy_name = 'betray everytime strategy'
 strategy_description = 'Betray every time'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
+    if 1==1:
+        return 'b' 
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
@@ -26,7 +22,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
