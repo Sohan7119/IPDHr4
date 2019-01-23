e17c9320186e095a5358b47d06f3f99c1e20d273
Bradford Pratt <235575@c1mww59bj1wk.isd720.com>
2019-01-23 12:30:43 -0600

    Updated Team Name

--- team13.py
+++ team13.py
@@ -6,9 +6,13 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Bradford Pratts Team' # Only 10 chars displayed.
+strategy_name = 'Collude, Recompense, Backstab'
+strategy_description = '''I plan to do a strategy similar to 4 in which 
+I collude the first turn and every single turn after that. BUT, if the 
+other player decides to betray, then I will betray them for the next 5 turns. 
+And if it EVER happens, I plan to make one betrayal on the 200th round.
+'''
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,9 +30,16 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if len(my_history)==0:
+        return 'c'
+    elif 'b' in their_history or len(their_history)>199:
+        return 'b'
+    elif 'b' in their_history[-5:]:
+        return 'b'
+    else:
+        return 'c'
+        
 
-    
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
@@ -53,7 +64,7 @@ if __name__ == '__main__':
               my_score=0,
               their_score=0,
               result='b'):
-         print('Test passed')
+        print ('Test passed')
      # Test 2: Continue betraying if they collude despite being betrayed.
     test_move(my_history='bbb',
               their_history='ccc', 
@@ -66,3 +77,4 @@ if __name__ == '__main__':
               my_score=0, 
               their_score=0,
               result='b')             
+              
